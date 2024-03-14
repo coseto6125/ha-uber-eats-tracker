5838e9cea0c7de9fc30f5cb3abf2dd13d12f8fd9
E-NoR <enorenor@gmail.com>
2024-03-14 21:06:50 +0800

    update api

--- custom_components/uber_eats/api.py
+++ custom_components/uber_eats/api.py
@@ -10,13 +10,13 @@ class UberEatsApi:
 
     def __init__(self, token):
         self._sid = token
-        self._locale_code = "TW"
+        # self._locale_code = "TW"
         self._timezone = "Asia/Taipei"
-        self._url_base = f"https://www.ubereats.com/api/getActiveOrdersV1?localeCode={self._locale_code}"
+        self._url_base = "https://www.ubereats.com/api/getActiveOrdersV1" # ?localeCode={self._locale_code}"
 
     def get_deliveries(self):
         headers = {"Content-Type": "application/json", "X-CSRF-Token": "x", "Cookie": f"sid={self._sid}"}
-
+        headers |= {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
         data = {"orderUuid": None, "timezone": self._timezone, "showAppUpsellIllustration": True}
         response = post(self._url_base, headers=headers, json=data)
         data = {"data": "Failed to fetch deliveries."}
