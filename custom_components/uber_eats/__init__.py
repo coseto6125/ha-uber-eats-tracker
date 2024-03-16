9c7515248aaae7bf684c3c26fbfbd33638f37ade
E-NoR <enorenor@gmail.com>
2024-03-17 02:03:47 +0800

    update setting

--- custom_components/uber_eats/__init__.py
+++ custom_components/uber_eats/__init__.py
@@ -8,6 +8,6 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry):
     """Set up Uber Eats as config entry."""
-    hass.data[DOMAIN] = UberEatsApi(entry.data["sid_token"])
+    hass.data[DOMAIN] = UberEatsApi(entry.data.get("sid_token"))
     hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
     return True
